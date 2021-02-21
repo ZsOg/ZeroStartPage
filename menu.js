90a69dea612c787f41ae6f5ae75958f0af14a03d
StarGolx <StarGolx@users.noreply.github.com>
2021-02-21 17:13:36 +0100

    Update

--- menu.js
+++ menu.js
@@ -14,6 +14,22 @@ $(document).ready(function() {
     $("#content").click(function(event) {
         $(".on").removeClass('on');
         $(".list").addClass('closed');
-        $('#word').hide();
     });
 });
+var list = document.getElementById("list")
+var showHide = document.getElementById("showHide")
+showHide.onclick = function() {
+    if (list.offsetLeft == 0) {
+        list.style['margin-left'] = -300 + "px"
+    } else {
+        list.style['margin-left'] = 0 + "px"
+    }
+}
+function showHide() {
+    if (list.offsetLeft == 1066) {
+        list.setAttribute("class", "closed")
+    } else {
+        list.setAttribute("class", "opened")
+        console.log("not in")
+    }
+}
