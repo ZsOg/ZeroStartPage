9de47dcab51d32e7ae6167f682e64db32c068ebf
StarGolx <StarGolx@users.noreply.github.com>
2021-02-22 09:35:49 +0100

    Update again(X2)

--- menu.js
+++ menu.js
@@ -1,35 +1,34 @@
 $(document).ready(function() {
-    //判断窗口大小，添加输入框自动完成
-    var wid = $("body").width();
-    if (wid < 640) {
-        //$(".wd").attr('autocomplete', 'off');
-    } else {
-        $(".wd").focus();
-    }
-    //菜单点击
-    $("#menuBtn").click(function(event) {
-        $(this).toggleClass('on');
-        $(".list").toggleClass('closed');
-    });
-    $("#content").click(function(event) {
-        $(".on").removeClass('on');
-        $(".list").addClass('closed');
-    });
+  //判断窗口大小，添加输入框自动完成
+  var wid = $("body").width();
+  if (wid < 640) {
+    //$(".wd").attr('autocomplete', 'off');
+  } else {
+    $(".wd").focus();
+  }
+  //菜单点击
+  $("#menuBtn").click(function(event) {
+    $(this).toggleClass('on');
+    $(".list").toggleClass('closed');
+  });
+  $("#content").click(function(event) {
+    $(".on").removeClass('on');
+    $(".list").addClass('closed');
+  });
 });
 var menu = document.getElementById("menu")
-var showHide = document.getElementById("showHide")
-showHide.onclick = function() {
-    if (menu.offsetLeft == 0) {
-        menu.style['margin-left'] = -300 + "px"
-    } else {
-        menu.style['margin-left'] = 0 + "px"
-    }
+var showMenu = document.getElementById("showMenu")
+showMenu.onclick = function() {
+  if (menu.offsetLeft == 0) {
+    menu.style['margin-left'] = -300 + "px"
+  } else {
+    menu.style['margin-left'] = 0 + "px"
+  }
 }
-function showHide() {
-    if (list.offsetLeft == 1066) {
-        list.setAttribute("class", "closed")
-    } else {
-        list.setAttribute("class", "opened")
-        console.log("not in")
-    }
+function showMenu() {
+  list.setAttribute("class", "list opened")
+  console.log("not in")
+}
+function hideMenu() {
+  list.setAttribute("class", "list closed")
 }
